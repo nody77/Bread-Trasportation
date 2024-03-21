377a0744835738b077a97bbc9568d36ee4b1e9f5
Nada <104033293+nody77@users.noreply.github.com>
2024-03-21 11:53:02 +0200

    calculating the work done but gives exception in the last test

--- BreadTransportaion/PROBLEM_CLASS.cs
+++ BreadTransportaion/PROBLEM_CLASS.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Problem
 {
@@ -25,7 +27,66 @@ namespace Problem
         static public  Int64 RequiredFunction(int N,int[] DemandPerHouse)
         {
             //REMOVE THIS LINE BEFORE START CODING
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            int work = 0;
+            List<int> suppliers = new List<int>();
+            List<int> demanders = new List<int>();
+
+            Parallel.For(0, N, i =>
+            {
+                if (DemandPerHouse[i] > 0)
+                {
+                    suppliers.Add(i);
+                }
+                else if (DemandPerHouse[i] < 0)
+                {
+                    demanders.Add(i);
+                }
+            });
+
+            /*for (int i = 0; i < N; i++)
+            {
+                if (DemandPerHouse[i] > 0)
+                {
+                    suppliers.Add(i);
+                }
+                else if (DemandPerHouse[i] < 0)
+                {
+                    demanders.Add(i);
+                }
+            }*/
+
+            int s = 0, d = 0;
+            while (s < suppliers.Count && d < demanders.Count)
+            {
+                int distance, trade = 0;
+                if(suppliers[s] - demanders[d] < 0)
+                {
+                    distance = -1 * (suppliers[s] - demanders[d]);
+                }
+                else
+                {
+                    distance = suppliers[s] - demanders[d];
+                }
+                if (DemandPerHouse[suppliers[s]] <= -1 * DemandPerHouse[demanders[d]])
+                {
+                    trade = DemandPerHouse[suppliers[s]];
+                }
+                else if (DemandPerHouse[suppliers[s]] > -1 * DemandPerHouse[demanders[d]])
+                {
+                    trade = DemandPerHouse[demanders[d]];
+                }
+                work += trade * distance;
+
+                DemandPerHouse[suppliers[s]] -= trade;
+                DemandPerHouse[demanders[d]] += trade;
+
+                if (DemandPerHouse[suppliers[s]] == 0) s++;
+                if (DemandPerHouse[demanders[d]] == 0) d++;
+            }
+
+            return work;
         }
         #endregion
     }
